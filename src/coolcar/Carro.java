64eda6288f8d245a313311fc7edf554376415f20
kazuyaym <kazuya.yamazaki@gmai.com>
2015-11-01 12:27:11 -0200

    criacao de moto e carro

---
+++ src/coolcar/Carro.java
@@ -0,0 +1,22 @@
+package coolcar;
+
+public class Carro extends Modelo {
+	  Carro() {
+	  }
+	  
+	  public boolean getTipoCar() {
+		  return true;
+	}
+
+	  public int getNumeroAssentos() {
+		  return 5;
+	  }
+	  
+	  public boolean getCambio() {
+		  return true;
+	  }
+	  
+	  public boolean getArCond() {
+		  return true;
+	  }
+}
