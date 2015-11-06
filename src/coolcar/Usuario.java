d58c533735b73f263d870d7f4233e35b2a224c37
rodrigozerbini <rodrigozerbini@Rodrigos-MacBook-Pro.local>
2015-11-06 16:34:38 -0200

    cadastro de novos usuarios

---
+++ src/coolcar/Usuario.java
@@ -0,0 +1,22 @@
+package coolcar;
+
+public class Usuario {
+
+	String nome, sobrenome;
+	String id;
+	String email, password;
+	
+	Usuario(String nome, String sobrenome, String id, String email, String password) {
+		
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		
+		System.out.println("Dados do usuario: " + nome + sobrenome);
+		System.out.println("id " + id);
+		System.out.println("email " + email);
+		System.out.println("password " + password);
+	}
+}
