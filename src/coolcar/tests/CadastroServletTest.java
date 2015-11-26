5238480211b5a736ff91f38a96a0ca3b07c6c428
Tiago Madeira <tmadeira@gmail.com>
2015-11-26 10:25:28 -0200

    Adiciona stub para testar CadastroServlet

---
+++ src/coolcar/tests/CadastroServletTest.java
@@ -0,0 +1,14 @@
+package coolcar.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadastroServletTest {
+
+  @Test
+  public void testDoPostHttpServletRequestHttpServletResponse() {
+    fail("Not yet implemented");
+  }
+
+}
