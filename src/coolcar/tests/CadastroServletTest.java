bd647ecf996a298621edbd832feecfbd68b7b33a
Felipe Túlio <felipe.tulio.pc@gmail.com>
2015-11-26 12:07:52 -0200

    Inserção do Mockito no projeto e teste base do servlet de cadastro

--- src/coolcar/tests/CadastroServletTest.java
+++ src/coolcar/tests/CadastroServletTest.java
@@ -1,14 +1,28 @@
 package coolcar.tests;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+import javax.servlet.http.*;
 
-public class CadastroServletTest {
+import coolcar.servlets.CadastroServlet;
+
+public class CadastroServletTest{
 
   @Test
-  public void testDoPostHttpServletRequestHttpServletResponse() {
-    fail("Not yet implemented");
+  public void testTelefoneErrado() throws Exception{
+	  HttpServletRequest request = mock(HttpServletRequest.class);       
+      HttpServletResponse response = mock(HttpServletResponse.class);
+      
+      when(request.getParameter("telefone")).thenReturn("12345");
+
+      new CadastroServlet().doPost(request, response);
+      
+      verify(request, atLeast(1)).getParameter("telefone");
+      
+      verify(response, never()).sendRedirect("contaCriada.jsp");
+      
   }
 
 }
