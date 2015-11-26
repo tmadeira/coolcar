14df6ca1bc5cf710aad1d7c01904b59c079e0866
Tiago Madeira <tmadeira@gmail.com>
2015-11-26 10:18:45 -0200

    Adiciona stubs para testes da Sessao e coloca JUnit no classpath

---
+++ src/coolcar/tests/SessaoTest.java
@@ -0,0 +1,34 @@
+package coolcar.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SessaoTest {
+
+  @Test
+  public void testLogIn() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testLogOut() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsLogged() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetNomeUsuario() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetId() {
+    fail("Not yet implemented");
+  }
+
+}
